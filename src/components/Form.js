d7bb3c9555d5f00418b887293266fff9cb926ed7
stanford kangethe <stanfordkangethe16@gmail.com>
2023-06-29 06:34:41 +0300

    Controlled forms code along

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form(props) {
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
+  // const [newsletter, setNewsletter] = useState(false)
+
+  // function handleFirstNameChange(event) {
+  //   setFirstName(event.target.value)
+  // }
+
+  // function handleLastNameChange(event) {
+  //   setLastName(event.target.value)
+  // }
+
+  // function handleNewsletterChange(event) {
+  //   setNewsletter(event.target.checked)
+  // }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={props.handleFirstNameChange} value={props.firstName} />
+      <input type="text" onChange={props.handleLastNameChange} value={props.lastName} />
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input 
+        type="checkbox"
+        id="newsletter"
+        onChange={props.handleNewsletterChange}
+        checked={props.newsletter}/>
       <button type="submit">Submit</button>
     </form>
   );
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import Form from "./Form"
+
+const ParentComponent = () => {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+  const [newsletter, setNewsletter] = useState(false)
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value)
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value)
+  }
+
+  function handleNewsletterChange(event) {
+    setNewsletter(event.target.checked)
+  }
+
+  return (
+    <Form 
+      firstName={firstName}
+      lastName={lastName}
+      newsletter={newsletter}
+      handleFirstNameChange={handleFirstNameChange}
+      handleLastNameChange={handleLastNameChange}
+      handleNewsletterChange={handleNewsletterChange}
+    />
+  )
+}
+
+export default ParentComponent
